f610e3edec05d7759a4d61ccfb8e6ad44c32979d
anderecc <andersondbl06@gmail.com>
2023-05-12 12:26:52 -0300

    update 3.0.1

--- 3-recboy/src/functions/verifyEmpty.js
+++ 3-recboy/src/functions/verifyEmpty.js
@@ -1,9 +1,7 @@
 export default function verifyEmpty(value) {
     if (value) {
-        console.log(true);
         return true;
     } else {
-        console.log(false);
         return false;
     }
 }
